72b719249d877f9fd0641ada0d433470445e908b
Raveen Jayasinghe <raveenj95@gmail.com>
2024-01-21 09:33:42 +0000

    add custom spam for honeycomb

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -3,8 +3,6 @@ from opentelemetry import trace
 
 class HomeActivities:
   def run():
-    current_span = trace.get_current_span()
-
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
@@ -45,9 +43,13 @@ class HomeActivities:
       'replies': []
     }
     ]
-
+    
+    current_span = trace.get_current_span()
     current_span.set_attribute("request.time", str(now))
-    current_span.set_attribute("response.body", str(results))
-    current_span.set_attribute("result.count", len(results))
 
+    tracer = trace.get_tracer(__name__)
+    with tracer.start_as_current_span("response") as span:
+      span.set_attribute("response.item_count", len(results))
+      span.set_attribute("response.body", str(results))
+      
     return results
