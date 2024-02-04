b4b8f01da6e398e62c06dc45da8835e01ba7ac9b
Raveen Jayasinghe <raveenj95@gmail.com>
2024-02-04 11:28:40 +0000

    update

--- frontend-react-js/src/configs/cognito.js
+++ frontend-react-js/src/configs/cognito.js
@@ -3,7 +3,7 @@ import { Amplify } from 'aws-amplify';
 Amplify.configure({
     Auth: {
       region: process.env.REACT_APP_AWS_COGNITO_REGION,
-      userPoolId: process.env.REACT_APP_AWS_CONGNITO_USER_POOLS_ID,
+      userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOLS_ID,
       userPoolWebClientId: process.env.REACT_APP_AWS_COGNITO_APP_CLIENT_ID,
     }
 });
